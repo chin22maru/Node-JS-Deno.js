c0047c75a8c50e9f2beb45cb9b3aeaf87b469a79
chin22maru <chinmaru2004@gmail.com>
2025-02-26 14:56:19 +0530

    Added the product Id to the routes, extracting dynamic params, loaded product detail data, rendering the product detail view, passing dat with post requests, adding cart model

---
+++ 09 Dynamic Routes and Advanced models/119to122 route params/models/product.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = callBack => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imgUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callBack) {
+    getProductsFromFile(callBack);
+  }
+
+  static findById(id, callBack){
+    getProductsFromFile( products => {
+      const product = products.find( p => p.id === id);
+      callBack(product);
+    });
+  }
+};
