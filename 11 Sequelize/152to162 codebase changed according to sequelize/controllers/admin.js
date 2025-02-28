f26a4603d6c27dfd3146aa33d50eeeb4d70803d5
chin22maru <chinmaru2004@gmail.com>
2025-02-28 17:41:23 +0530

    connets with db, define the user and product model, sync js def to the db, insert data and create product, retrieve data and find products, fetch-update-delete admin products

---
+++ 11 Sequelize/152to162 codebase changed according to sequelize/controllers/admin.js
@@ -0,0 +1,113 @@
+const Product = require('../models/product')
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('admin/edit-product', {
+        pageTitle: 'Add Product', 
+        Path: './admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        editing: false
+    })
+}
+  
+exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imgUrl = req.body.imgUrl;
+    const description = req.body.description;
+    const price = req.body.price;
+
+    Product.create({
+      title: title,
+      price: price,
+      imgUrl: imgUrl,
+      description: description,
+    })
+    .then( result => {
+      // console.log(result);
+      console.log('created a product!');
+      res.redirect('/admin/products');
+    })
+    .catch( err => {
+      console.log('Error: .............');
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editmode = req.query.editmode;
+  if(!editmode){
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+
+  Product.findByPk(prodId)
+    .then( product => {
+        if(!product){
+          return res.redirect('/');
+        }
+    
+        res.render('admin/edit-product', {
+          editing: editmode,
+          product:product,
+          pageTitle: 'Edit Product', 
+          Path: './admin/edit-product',
+          formsCSS: true,
+          productCSS: true
+        })
+    })
+    .catch( err => console.log(err) );
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;  // in body we have name-value prameters
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedimgUrl = req.body.imgUrl;
+
+  Product.findByPk(prodId)
+    .then( product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imgUrl = updatedimgUrl;
+      return product.save();
+    })
+    .then( result => {
+      console.log("Product Updated!");
+      res.redirect('/admin/products');
+    })
+    .catch( err => console.log(err) );
+
+}
+
+  exports.getProducts = (req, res, next) => {
+    Product.findAll()
+      .then( products => {
+        res.render('admin/products', {
+          prods: products, 
+          pageTitle: 'Admin Products', 
+          Path: './admin/products', 
+          formsCSS: false,
+          productCSS: true
+        }); 
+      })
+      .catch( err => console.log(err));
+  }
+
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.findByPk(prodId)
+    .then( product => {
+      return product.destroy();
+    })
+    .then( result => {
+      console.log("Product Deleted!");
+      res.redirect('/admin/products');
+    })
+    .catch( err => console.log(err) );
+}
+  
---
+++ 11 Sequelize/152to162 codebase changed according to sequelize/controllers/shop.js
@@ -0,0 +1,109 @@
+const Product = require('../models/product')
+const Cart = require('../models/cart')
+
+exports.getProducts = (req, res, next) => {
+    Product.findAll()
+    .then( products => {
+        res.render('shop/product-list', {
+          prods: products, 
+          pageTitle: 'All Products', 
+          Path: '/products', 
+          formsCSS: false,
+          productCSS: true
+        }); // use default templating engine(pug) provided by app.set and return the template from path provided in app.set's views(/views) property : here in views folder shop.pug
+    })
+    .catch( err => {
+      console.log(err);
+    });
+}
+
+exports.getProduct = (req,res,next) => {
+  const prodId = req.params.productId;
+  // Product.findByPk(prodId)
+  Product.findAll({where: {id: prodId}})
+    .then( products => {
+        res.render('shop/product-detail', {
+          product: products[0], 
+          pageTitle: products[0].title,
+          Path: '/products',
+        });
+    })
+    .catch( err => console.log(err));
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+    .then( products => {
+      res.render('shop/index', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        Path: '/', 
+        formsCSS: false,
+        productCSS: true
+      }); 
+    })
+    .catch( err => console.log(err));
+}
+
+exports.getCart = (req,res,next) => {
+  Cart.getCart( cart => {
+    Product.fetchAll( products => {
+      const cartProducts = [];
+      for(let product of products){
+        const cartProduct = cart.products.find(prod => prod.id===product.id);
+        if(cartProduct) cartProducts.push({product, qty: cartProduct.qty});
+      }
+
+      res.render('shop/cart',{
+        Path: '/cart',
+        pageTitle: 'Your Cart',
+        cartProducts,
+      });
+    });
+  });
+}
+
+// exports.postCart = (req,res,next) => {
+//   const prodId = req.body.productId;
+//   Product.findById(prodId, (product) => {
+//     Cart.addProduct(prodId, product.price);
+    
+//   })
+//   res.redirect('/cart');
+// }
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then( ([rows]) => {
+      Cart.addProduct(prodId, rows[0].price, () => {
+        res.redirect('/cart');
+      });
+    })
+    .catch( err => console.log(err));
+};
+
+exports.postCartDeleteItem = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then( ([rows]) => {
+      Cart.deleteProduct(prodId, rows[0].price);
+      res.redirect('/cart');
+    })
+    .catch( err => console.log(err));
+};
+
+
+exports.getOrders = (req,res,next) => {
+  res.render('shop/orders',{
+    Path: '/orders',
+    pageTitle: 'Your Orders',
+  });
+}
+
+exports.getCheckout = (req,res,next) => {
+  res.render('shop/checkout',{
+    Path: '/checkout',
+    pageTitle: 'Checkout',
+  });
+}
